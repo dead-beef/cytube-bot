f71c50b10716b9431e7d897f71264fad8f29c75e
dead-beef <dead-beef@users.noreply.github.com>
2018-07-26 09:49:21 +0000

    add tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,16 +1,49 @@
 import asyncio
 import pytest
 
-from cytube_bot.util import Queue
+from cytube_bot.util import Queue, to_sequence, cloak_ip, uncloak_ip
 
 
 @pytest.mark.asyncio
 @asyncio.coroutine
 def test_queue(event_loop):
-    q = Queue(loop=event_loop)
-    assert isinstance(q, asyncio.Queue)
-    yield from q.put(0)
-    res = yield from q.get()
+    queue = Queue(loop=event_loop)
+    assert isinstance(queue, asyncio.Queue)
+    yield from queue.put(0)
+    res = yield from queue.get()
     assert res == 0
-    q.task_done()
-    yield from q.join()
+    queue.task_done()
+    yield from queue.join()
+
+
+@pytest.mark.parametrize('test,res', [
+    (None, ()),
+    (0, (0,)),
+    ('str', ('str',)),
+    ({'x': 0}, ({'x': 0},)),
+    ((0, 1), None),
+    ([0, 1], None)
+])
+def test_to_sequence(test, res):
+    if res is None:
+        assert to_sequence(test) is test
+    else:
+        assert to_sequence(test) == res
+
+
+@pytest.mark.parametrize('test,res', [
+    (('127.0.0.1',), 'yFA.j8g.iXh.gvS'),
+    (('127.0.0.1', 2), '127.0.ou9.RBl')
+])
+def test_cloak_ip(test, res):
+    assert cloak_ip(*test) == res
+
+
+@pytest.mark.parametrize('test,res', [
+    (('yFA.j8g.iXh.gvS',), ['127.0.0.1']),
+    (('127.0.ou9.RBl',), []),
+    (('127.0.ou9.RBl', 2), ['127.0.0.1']),
+    (('127.0.ou9.RBl', None), ['127.0.0.1'])
+])
+def test_uncloak_ip(test, res):
+    assert uncloak_ip(*test) == res
